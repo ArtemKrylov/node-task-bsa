ebd63a09fa53bc87d2c3693ff2985008841600b3
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-25 22:32:19 +0300

    feat: added onFightStart function in client, fightRoutes and fightService - create fight and set winner

--- routes/fightRoutes.js
+++ routes/fightRoutes.js
@@ -10,4 +10,43 @@ const router = Router();
 
 // OPTIONAL TODO: Implement route controller for fights
 
+router.post(
+  "",
+  (req, res, next) => {
+    try {
+      const data = req.body;
+      const fight = fightersService.createFight(data);
+      res.send(fight);
+    } catch (error) {
+      res.status(400).send({
+        error: true,
+        message: "Fight request is not valid",
+      });
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.post(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const winner = req.body;
+      const result = fightersService.finishFight(id, winner);
+      res.send(result);
+    } catch (error) {
+      res.status(400).send({
+        error: true,
+        message: error.message,
+      });
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
 export { router };
