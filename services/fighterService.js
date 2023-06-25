72c445dbd7d94d91fb9b6f69d57b94199cbd8d90
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-06-25 13:04:17 +0300

    feat: added fighterRoutes, fighterService and fighter validation

--- services/fighterService.js
+++ services/fighterService.js
@@ -6,8 +6,23 @@ class FighterService {
   createFighter(data) {
     return fighterRepository.create(data);
   }
+  updateFighter(data) {
+    const foundFighter = fighterRepository
+      .getAll()
+      .find((fighter) => fighter.name === data.name);
+    const id = foundFighter.id;
+    return fighterRepository.update(id, data);
+  }
   getAllFighters() {
     return fighterRepository.getAll();
+  }
+  findFigther(data) {
+    return fighterRepository.getOne(data);
+  }
+  doesFighterExist(data) {
+    return Boolean(
+      fighterRepository.getAll().find((el) => el.name === data.name)
+    );
   }
 }
 
